7bba7a6b47b98a5629bed5c3e12e00672855dac6
suuungwoo <suuungwoo@gmail.com>
2019-12-01 00:20:47 +0900

    update

--- AtCoder Beginners Selection/ABC085C.py
+++ AtCoder Beginners Selection/ABC085C.py
@@ -2,9 +2,9 @@
 
 N, Y = map(int, input().split())
 x, y, z = -1, -1, -1
-for i in range(N+1):
-    for j in range(N-i+1):
-        if 10000*i + 5000*j + 1000*(N-i-j) == Y:
-            x, y, z = i, j, N-i-j
+for i in range(N + 1):
+    for j in range(N - i + 1):
+        if 10000 * i + 5000 * j + 1000 * (N - i - j) == Y:
+            x, y, z = i, j, N - i - j
             break
 print(x, y, z)
